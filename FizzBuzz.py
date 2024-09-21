8f62c8c72b1ffdd863078f7abbeaf26ce582d767
CHIRAGGARG1697 <chiraggarg.0106@gmail.com>
2024-09-21 14:33:53 +0530

    First Commit

---
+++ FizzBuzz.py
@@ -0,0 +1,15 @@
+for i in range(1,101):
+    if i % 3 == 0 and i%5 == 0:
+        print("FIZZBUZZ")
+
+    elif i % 3 == 0:
+        print("FIZZ")
+
+    elif i % 5 == 0 :
+        print("BUZZ")
+
+    else: #i% 3 != 0 and i%5 != 0 and i%15 != 0:
+        print(i)
+
+
+
